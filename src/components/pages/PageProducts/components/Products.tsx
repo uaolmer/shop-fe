c0b4123efd8ed1451987f1068c17ebf69f879415
Daniil <uaolmer2@gmail.com>
2021-08-29 22:14:13 +0300

    fix: products endpoint

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -36,7 +36,7 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    axios.get(`${API_PATHS.bff}/product/available/`)
+    axios.get(`${API_PATHS.bff}products/`)
       .then(res => setProducts(res.data));
     //setProducts(productList);
   }, [])
