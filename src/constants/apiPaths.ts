6d4f366a04c57361e881891b3555c6e88133b506
Daniil <uaolmer2@gmail.com>
2021-08-30 00:24:26 +0300

    fix: api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/',
-  order: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/',
-  import: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/',
-  bff: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/'
+  product: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
+  order: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
+  bff: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/'
 };
 
 export default API_PATHS;
