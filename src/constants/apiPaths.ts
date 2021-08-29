bdb444f3828bd8e5ac6736df51b572eda455db1f
Daniil <uaolmer2@gmail.com>
2021-08-29 20:30:28 +0300

    init: create task-3 template

---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,9 @@
+
+const API_PATHS = {
+  product: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/',
+  order: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/',
+  bff: 'https://o2wslad445.execute-api.eu-west-1.amazonaws.com/dev/'
+};
+
+export default API_PATHS;
