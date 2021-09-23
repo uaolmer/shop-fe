980a0dc61847f7d83fdf663a6664e5ddb56f5d1c
Daniil <uaolmer2@gmail.com>
2021-09-23 16:45:59 +0300

    feat: add import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
   order: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
-  import: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://jc17nqcl6l.execute-api.eu-west-1.amazonaws.com/dev/import/',
   bff: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/'
 };
 
