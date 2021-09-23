b8397f375849d22918e338a12f7f432e11ea8585
Daniil <uaolmer2@gmail.com>
2021-09-24 02:15:01 +0300

    feat: connect fe to be

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
-  order: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
+  product: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://bmb0ksna5l.execute-api.eu-west-1.amazonaws.com/dev/import',
-  bff: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev/',
+  bff: 'https://3dnlnnwq93.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
